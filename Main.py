2d6140b33454dc2842a2a9b98121a60e5aecd60a
Berkay <wberkayw@gmail.com>
2024-03-22 06:16:30 +0100

    task 3 is done.

--- Main.py
+++ Main.py
@@ -1,17 +1,30 @@
+class SquareGenerator:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def generate(self):
+        return [i ** 2 for i in range(self.start, self.end + 1)]
+
+
+def task2(start, end):
+    print("~~~~~~~~~~~~~~~~~~~~~~ Task 2 ~~~~~~~~~~~~~~~~~~~~~~")
+    result = [i ** 2 for i in range(start, end + 1)]
+    print(result)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+
 def main():
+    print("~~~~~~~~~~~~~~~~~~~~~~ Task 1 ~~~~~~~~~~~~~~~~~~~~~~")
+    squares_list = [i ** 2 for i in range(1, 11)]
+    print(squares_list)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
     task2(5, 20)
 
-
-print("~~~~~~~~~~~~~~~~~~~~~~ Task 1 ~~~~~~~~~~~~~~~~~~~~~~")
-x = [i ** 2 for i in range(1, 11)]
-print(x)
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-
-def task2(x, y):
-    print("~~~~~~~~~~~~~~~~~~~~~~ Task 2 ~~~~~~~~~~~~~~~~~~~~~~")
-    x = [i ** 2 for i in range(x, y+1)]
-    print(x)
+    print("~~~~~~~~~~~~~~~~~~~~~~ Task 3 ~~~~~~~~~~~~~~~~~~~~~~")
+    square_generator = SquareGenerator(10, 50)
+    print(square_generator.generate())
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
